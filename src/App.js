18d8a962a51df887005063211c389124d9c6e4d5
kdilot <kdilot@gmail.com>
2018-07-23 14:28:19 -0700

    react example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
-import Counter from './Counter';
+import Phoneform from './component/Phoneform';
+import PhoneFormList from './component/PhoneInfoList';
 import './App.css';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: 'Tom',
+        phone: '123-345-3454'
+      },
+      {
+        id: 1,
+        name: 'Jerry',
+        phone: '345-643-2234'
+      },
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
   render() {
+    const { information } = this.state;
     return (
       <div>
-        <div className="App">
-          React
-        </div>
-        <MyName name="React"/>
-        <Counter />
+        <Phoneform onCreate={this.handleCreate} />
+        <PhoneFormList data={this.state.information} />
       </div>
     );
   }
