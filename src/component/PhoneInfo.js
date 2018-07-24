702d7be932afd88a1fe5dfb5dca1d0db1a2f498a
kdilot <kdilot@gmail.com>
2018-07-24 14:17:08 -0700

    intput box modify & delete example

--- src/component/PhoneInfo.js
+++ src/component/PhoneInfo.js
@@ -8,15 +8,95 @@ class PhoneInfo extends Component {
             id: 0
         }
     }
+
+    state = {
+        editing: false,
+        cancel: true,
+        name: '',
+        phone: ''
+    }
+
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+
+    handleToggleEdit = () => {
+        const { editing } = this.state;
+        this.setState({ editing: !editing, cancel: true })
+    }
+
+    handleCancel = () => {
+        const { editing } = this.state;
+        this.setState({ editing: !editing, cancel: false })
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { info, onUpdate } = this.props;
+        if (!prevState.editing && this.state.editing) {
+            this.setState({
+                name: info.name,
+                phone: info.phone
+            })
+        }
+        if (prevState.editing && !this.state.editing) {
+            onUpdate(info.id, {
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }
+        if (prevState.cancel && !this.state.cancel) {
+            onUpdate(info.id, {
+                name: info.name,
+                phone: info.phone
+            });
+        }
+    }
+
     render() {
+        const { editing } = this.state;
+        if (editing) {
+            return (
+                <div className="List">
+                    <div>
+                        <input
+                            value={this.state.name}
+                            name="name"
+                            placeholder="name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value={this.state.phone}
+                            name="phone"
+                            placeholder="phone"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button onClick={this.handleToggleEdit}>application</button>
+                    <button onClick={this.handleCancel}>cancel</button>
+                </div>
+            );
+        }
         const {
-            name, phone, id
+            name, phone
         } = this.props.info;
 
         return (
             <div className="List">
                 <div><b>{name}</b></div>
                 <div>{phone}</div>
+                <button onClick={this.handleToggleEdit}>modify</button>
+                <button onClick={this.handleRemove}>delete</button>
             </div>
         );
     }
--- src/component/PhoneInfoList.js
+++ src/component/PhoneInfoList.js
@@ -3,15 +3,16 @@ import PhoneInfo from './PhoneInfo';
 
 class PhoneInfoList extends Component {
     static defaultProps = {
-        data: []
+        list: [],
+        onRemove: () => console.warn('onRemove not defined'),
+        onUpdate: () => console.warn('onUpdate not defined'),
     }
 
     render() {
-        const { data } = this.props;
+        const { data, onRemove, onUpdate } = this.props;
         const list = data.map(
-            (info, index) => (<PhoneInfo key={index} info={info} />)
+            (info, index) => (<PhoneInfo key={index} info={info} onRemove={onRemove} onUpdate={onUpdate} />)
         );
-        console.log(list);
         return (
             <div>
                 {list}
